b5880747dac9c02f41bed5722738a09e0bf0b910
kanathip sae-pae <s7070013@kmitl.ac.th>
2017-03-27 22:40:02 +0700

    Hello World

--- src/main/java/com/kanathips/learning/rxjava/RxMain.java
+++ src/main/java/com/kanathips/learning/rxjava/RxMain.java
@@ -1,7 +1,22 @@
 package com.kanathips.learning.rxjava;
 
+import rx.Observable;
+import rx.functions.Action1;
+
 /**
  * Created by new_z on 27/03/2017.
  */
 public class RxMain {
+    public static void main(String... args){
+        hello("kanathip", "salinee", "albert");
+    }
+
+    public static void hello(String... names) {
+        Observable.from(names).subscribe(new Action1<String>() {
+
+            public void call(String s) {
+                System.out.println("Hello " + s + "!");
+            }
+        });
+    }
 }
